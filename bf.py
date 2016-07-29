3cc18f5b8166972ee417e62f792e90582d2278c7
Gabriel Bizzotto <ok@ok.com>
2016-07-29 13:55:49 -0300

    - revert array back to list - optimize [-]>[-] to d[0:2] = [0] * 2 and such - rename memory to d and pointer to p for no reason - optimize seek loops [>] to list.index() - replace multiplication by 1 or -1 by simple add or sub -

--- bf.py
+++ bf.py
@@ -3,13 +3,12 @@
 # - fix the "cat code | python bf.py" with no ! in code hanging problm
 # - more optimization
 # BFBench:
-# - factor.b:           16.94s (time echo "123456789" |  python bf.py factor.b)
-# - mandelbrot:       4m45
-# - si ! si ! hi123: 11m26
+# - factor.b:          12.8s (time echo "123456789" |  python bf.py factor.b)
+# - mandelbrot:      3m50
+# - si ! si ! hi123: 7m51
 
 import os.path
 import sys
-from array import array 
 import select
 
 def replace_subsequence_once(l,a,b):
@@ -25,17 +24,16 @@ def replace_subsequence(l,a,b):
       pass
 
 #@profile
-def RunInline(bf, fin, print_code, filename):
+def RunInline(bf, print_code, filename):
    code = ""
    code += "# " + filename + "\n\n"
    code += """import sys
-from array import array 
 
 fin = sys.stdin
 """
-   code += """memory = array('B', [0] * 10000)
-pointer = 0
-lenmemory = 1000
+   code += """d = [0] * 10000
+p = 0
+lend = 1000
 
 """
    lenbf = len(bf)
@@ -45,28 +43,29 @@ lenmemory = 1000
       inst = bf[pc]
       v = 1
       if inst in ['+', '-', '<', '>']:
-         pointer_local = 0 
+         p_local = 0 
          add_map = {}
          set_map = {}
+         set_map_reverse = {}
          v = 0
          while inst in ['<', '>', '+', '-', 'z']:
             if inst == '<':
-               pointer_local -= 1
+               p_local -= 1
                v += 1
             elif inst == '>':
-               pointer_local += 1
+               p_local += 1
                v += 1
             elif inst == '-':
-               if pointer_local not in add_map.keys():
-                  add_map[pointer_local] = -1
+               if p_local not in add_map.keys():
+                  add_map[p_local] = -1
                else:
-                  add_map[pointer_local] -= 1
+                  add_map[p_local] -= 1
                v += 1
             elif inst == '+':
-               if pointer_local not in add_map.keys():
-                  add_map[pointer_local] = 1
+               if p_local not in add_map.keys():
+                  add_map[p_local] = 1
                else:
-                  add_map[pointer_local] += 1
+                  add_map[p_local] += 1
                v += 1
             elif inst == 'z':
                local_sum = 0
@@ -77,36 +76,59 @@ lenmemory = 1000
                   else:
                      local_sum -= 1
                   v += 1
-               set_map[pointer_local] = local_sum
-               if pointer_local in add_map.keys():
-                  del add_map[pointer_local]
+               set_map[p_local] = local_sum
+               if local_sum not in set_map_reverse.keys():
+                  set_map_reverse[local_sum] = [p_local];
+               else: 
+                  set_map_reverse[local_sum].append(p_local);
+               if p_local in add_map.keys():
+                  del add_map[p_local]
             if pc+v < lenbf:
                inst = bf[pc+v]
             else:
                inst = ' '
+         for value, keys in set_map_reverse.iteritems():   
+            keys.sort()
+            following = 0
+            for i in range(1,len(keys)):
+               following += 1
+               if keys[i] != keys[i-1]+1:
+                  if following >= 2:
+                     code += "   "*depth
+                     code += "d[p+"+str(keys[i-following])+":p+"+str(keys[i-1]+1)+"] = ["+str(value)+"] * "+str(following)+"\n"
+                     for j in range(keys[i-following], keys[i-following]+following):
+                        del set_map[j]
+                  following = 0
+            else: 
+               following += 1
+               if following >= 3 and keys[-following]>=1:
+                  code += "   "*depth
+                  code += "d[p+"+str(keys[-following])+":p+"+str(keys[-1]+1)+"] = ["+str(value)+"] * "+str(following)+"\n"
+                  for j in range(keys[-following], keys[-following]+following):
+                     del set_map[j]
          for key, value in set_map.iteritems():
-            if key != pointer_local:
+            if key != p_local:
                code += "   "*depth
                if key == 0:
-                  code += "memory[pointer] = "+str(value)+"\n"
+                  code += "d[p] = "+str(value)+"\n"
                else:
-                  code += "memory[pointer+"+str(key)+"] = "+str(value)+"\n"
+                  code += "d[p+"+str(key)+"] = "+str(value)+"\n"
          for key, value in add_map.iteritems():
-            if value != 0 and key != pointer_local:
+            if value != 0 and key != p_local:
                code += "   "*depth
                if key == 0:
-                  code += "memory[pointer] = (memory[pointer] + "+str(value)+")&255\n"
+                  code += "d[p] = (d[p] + "+str(value)+")&255\n"
                else:
-                  code += "memory[pointer+"+str(key)+"] = (memory[pointer+"+str(key)+"] + "+str(value)+")&255\n"
-         if pointer_local != 0:
+                  code += "d[p+"+str(key)+"] = (d[p+"+str(key)+"] + "+str(value)+")&255\n"
+         if p_local != 0:
             code += "   "*depth
-            code += "pointer += " + str(pointer_local) + "\n"
-         if pointer_local in set_map.keys():
+            code += "p += " + str(p_local) + "\n"
+         if p_local in set_map.keys():
             code += "   "*depth
-            code += "memory[pointer] = "+str(set_map[pointer_local])+"\n"
-         if pointer_local in add_map.keys():
+            code += "d[p] = "+str(set_map[p_local])+"\n"
+         if p_local in add_map.keys():
             code += "   "*depth
-            code += "memory[pointer] = (memory[pointer] + "+str(add_map[pointer_local])+")&255\n"
+            code += "d[p] = (d[p] + "+str(add_map[p_local])+")&255\n"
       elif inst == ']':
          depth -= 1
          local_sum = 0
@@ -121,66 +143,86 @@ lenmemory = 1000
             if local_sum < 0 or local_sum > 255:
                mask = "&255"
             code += "   "*depth
-            code += "memory[pointer] = "+str(local_sum)+mask+"\n"
+            code += "d[p] = "+str(local_sum)+mask+"\n"
       elif inst == '[':
-         pointer_local = 0 
-         add_map = {0: 0}
+         p_local = 0 
+         add_map = {}
          zero_set = set()
-         while bf[pc+v] != ']' and bf[pc+v] in ['<', '>', '+', '-', 'z']:
+         while bf[pc+v] in ['<', '>', '+', '-', 'z']:
             if bf[pc+v] == '<':
-               pointer_local -= 1
+               p_local -= 1
             elif bf[pc+v] == '>':
-               pointer_local += 1
+               p_local += 1
             elif bf[pc+v] == '-':
-               if pointer_local not in add_map.keys():
-                  add_map[pointer_local] = -1
+               if p_local not in add_map.keys():
+                  add_map[p_local] = -1
                else:
-                  add_map[pointer_local] -= 1
+                  add_map[p_local] -= 1
             elif bf[pc+v] == '+':
-               if pointer_local not in add_map.keys():
-                  add_map[pointer_local] = 1
+               if p_local not in add_map.keys():
+                  add_map[p_local] = 1
                else:
-                  add_map[pointer_local] += 1
+                  add_map[p_local] += 1
             elif bf[pc+v] == 'z':
-               zero_set.add(pointer_local)
-               if pointer_local in add_map.keys():
-                  del add_map[pointer_local]
+               zero_set.add(p_local)
+               if p_local in add_map.keys():
+                  del add_map[p_local]
             v += 1
-         is_an_add = pointer_local in zero_set
-         if bf[pc+v] == ']' and (is_an_add or (pointer_local == 0 and 0 in add_map.keys() and add_map[0] == -1)):
+         is_an_add = p_local in zero_set
+         if bf[pc+v] == ']' and len(add_map) == 0 and len(zero_set) == 0 and p_local == 1:
+            # seek loop [>]
+            local_sum = 0
+            v += 1
+            while pc+v < lenbf and bf[pc+v] in ['>','<']:
+               if bf[pc+v] == '>':
+                  local_sum += 1
+               else:
+                  local_sum -= 1
+               v += 1
+            code += "   "*depth
+            if local_sum == 0:
+               code += "p += d[p:].index(0)\n"
+            else:
+               code += "p += d[p:].index(0) + "+str(local_sum)+"\n"
+         elif bf[pc+v] == ']' and (is_an_add or (p_local == 0 and 0 in add_map.keys() and add_map[0] == -1)):
             # add map or mul map
             code += "   "*depth
-            code += "if memory[pointer] != 0:\n"
+            code += "if d[p] != 0:\n"
             depth += 1
             for k in zero_set:
-               if k != pointer_local:
+               if k != p_local:
                   code += "   "*depth
                   if k == 0:
-                     code += "memory[pointer] = 0\n"
+                     code += "d[p] = 0\n"
                   else:
-                     code += "memory[pointer+"+str(k)+"] = 0\n"
+                     code += "d[p+"+str(k)+"] = 0\n"
             for k in add_map.keys():
-               if k != pointer_local:
+               if k != p_local:
                   if k in zero_set:
                      # this value is set
                      code += "   "*depth
                      if k == 0:
-                        code += "memory[pointer] = " + str(add_map[0]) + "&255\n"
+                        code += "d[p] = " + str(add_map[0]) + "&255\n"
                      else:
-                        code += "memory[pointer+"+str(k)+"] = " + str(add_map[k]) + "&255\n"
+                        code += "d[p+"+str(k)+"] = " + str(add_map[k]) + "&255\n"
                   elif is_an_add:
                      code += "   "*depth
                      if k == 0:
-                        code += "memory[pointer] = (memory[pointer]+"+str(add_map[0])+")&255\n"
+                        code += "d[p] = (d[p]+"+str(add_map[0])+")&255\n"
                      else:
-                        code += "memory[pointer+"+str(k)+"] = (memory[pointer+"+str(k)+"]+"+str(add_map[k])+")&255\n"
+                        code += "d[p+"+str(k)+"] = (d[p+"+str(k)+"]+"+str(add_map[k])+")&255\n"
                   else:
                      # is a mul
                      code += "   "*depth
                      if add_map[k] == 1:
-                        code += "memory[pointer+"+str(k)+"] = (memory[pointer+"+str(k)+"]+memory[pointer])&255\n"
+                        code += "d[p+"+str(k)+"] = (d[p+"+str(k)+"]+d[p])&255\n"
                      else:
-                        code += "memory[pointer+"+str(k)+"] = (memory[pointer+"+str(k)+"]+memory[pointer]*"+str(add_map[k])+")&255\n"
+                        if add_map[k] == 1:
+                           code += "d[p+"+str(k)+"] = (d[p+"+str(k)+"]+d[p])&255\n"
+                        elif add_map[k] == -1:
+                           code += "d[p+"+str(k)+"] = (d[p+"+str(k)+"]-d[p])&255\n"
+                        else:
+                           code += "d[p+"+str(k)+"] = (d[p+"+str(k)+"]+d[p]*"+str(add_map[k])+")&255\n"
             v += 1
             local_sum = 0
             while pc+v < lenbf and bf[pc+v] in ['+','-']:
@@ -189,29 +231,29 @@ lenmemory = 1000
                else:
                   local_sum -= 1
                v += 1
-            if pointer_local != 0:
+            if p_local != 0:
                code += "   "*depth
-               code += "pointer += "+str(pointer_local)+"\n"
+               code += "p += "+str(p_local)+"\n"
             if local_sum != 0:
                mask = ""
                if mask < 0 or mask > 255:
                   mask = "&255"
                depth -= 1
                code += "   "*depth
-               if pointer_local == 0:
-                  code += "memory[pointer] = "+str(local_sum)+mask+"\n"
+               if p_local == 0:
+                  code += "d[p] = "+str(local_sum)+mask+"\n"
                else:
-                  code += "memory[pointer+"+str(pointer_local)+"] = "+str(local_sum)+mask+"\n"
+                  code += "d[p+"+str(p_local)+"] = "+str(local_sum)+mask+"\n"
             else:
                code += "   "*depth
-               if pointer_local == 0:
-                  code += "memory[pointer] = 0\n"
+               if p_local == 0:
+                  code += "d[p] = 0\n"
                else:
-                  code += "memory[pointer+"+str(pointer_local)+"] = 0\n"
+                  code += "d[p+"+str(p_local)+"] = 0\n"
                depth -= 1
          else:
             code += "   "*depth
-            code += "while memory[pointer] != 0:\n"
+            code += "while d[p] != 0:\n"
             depth += 1
             v = 1
       elif inst == 'z':
@@ -223,13 +265,13 @@ lenmemory = 1000
                local_sum -= 1
             v += 1
          code += "   "*depth
-         code += "memory[pointer] = "+str(local_sum)+"\n"
+         code += "d[p] = "+str(local_sum)+"\n"
       elif inst == ',':
          code += "   "*depth
-         code += "memory[pointer] = ord(fin.read(1))\n"
+         code += "d[p] = ord(fin.read(1))\n"
       elif inst == '.':
          code += "   "*depth
-         code += "sys.stdout.write(chr(memory[pointer]))\n"
+         code += "sys.stdout.write(chr(d[p]))\n"
       code += "   "*depth
       code += "# "
       for c in bf[pc:pc+v]:
@@ -244,7 +286,7 @@ lenmemory = 1000
 def ReadCode(filename):
    result = []
    with open(filename, "r") as f:
-      code = array('c', f.read())
+      code = f.read()
    for c in code:
       if c in ['+','-','<','>','[',']','.',',']:
          result.append(c)
@@ -269,8 +311,8 @@ def main(argv):
    
    code = []
    for s in filter(os.path.isfile, argv):
-      code = ReadCode(s)
       filename = s
+      code = ReadCode(s)
       break
    else:
       filename = "<stdin>"
@@ -281,7 +323,7 @@ def main(argv):
          if c in ['+','-','<','>','[',']','.',',']:
             code.append(c)
    code = Optimize(code)
-   RunInline(array('c',code), sys.stdin, "-c" in argv, filename)
+   RunInline(code, "-c" in argv, filename)
       
 
 if __name__ == "__main__":
